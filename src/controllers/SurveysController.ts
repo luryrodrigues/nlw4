c86f52bc5da5a431b53a741120bb4bc1be4ce7ec
luryrodrigues <lurylrodrigues@gmail.com>
2021-02-25 21:06:29 -0300

    creating function show all

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -2,11 +2,7 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { SurveysRepository } from '../repositories/SurveysRepository';
 
-class SurveyController {
-  show(arg0: string, show: any) {
-    throw new Error('Method not implemented.');
-  }
-
+class SurveysController {
   async create(req: Request, res: Response) {
     const {title, description} = req.body;
 
@@ -14,12 +10,19 @@ class SurveyController {
 
     const survey = surveysRepository.create({
       title, description 
-    })
+    });
 
     await surveysRepository.save(survey);
 
     return res.status(201).json(survey);
   }
+  
+  async show(req: Request, res: Response) {
+    const surveysRepository = getCustomRepository(SurveysRepository);
+    
+    const all = await surveysRepository.find();
+  }
 }
 
-export { SurveyController };
+
+export { SurveysController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { UserController } from "./controllers/UserController";
-import { SurveyController } from "./controllers/SurveysController";
+import { SurveysController } from "./controllers/SurveysController";
 
 const router = Router();
 
 const userController = new UserController();
-const surveyController = new SurveyController();
+const surveysController = new SurveysController();
 
 router.post("/users", userController.create);
-router.post("/surveys", surveyController.create);
-router.get("/surveys", surveyController.show);
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router } ; 
